5ae5c2a0efd28c224b161793cd1dbd4572b680e5
Guudz <Kilian1702@outlook.com>
2023-02-02 01:34:22 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Leaking Hub Joshua Y.
+
+files {
+    "interiorproxies.meta"}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+this_is_a_map 'yes'
+
+fx_version 'adamant'
+games {'gta5'}
